93418626827d0a7d7c42f5d2a951c05fb5a2c9b5
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-07-04 16:06:55 +0300

    Add files via upload

---
+++ Проверка за просто число/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверка_за_просто_число
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int br = 0;
+
+            Console.Write("Въведете число: ");
+            n = int.Parse(Console.ReadLine());
+
+            for(int i = 1;i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    br++;
+                }
+            }
+
+            if (br == 2)
+            {
+                Console.WriteLine("Числото е просто");
+            }
+
+            else
+            {
+                Console.WriteLine("Числото не е просто");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
